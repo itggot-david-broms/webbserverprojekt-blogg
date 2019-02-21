fb64a3b6c11c6558a933a74d7e7382b79edfab15
itggot-david-broms <david.broms@itggot.se>
2019-02-21 10:54:32 +0100

    added register

---
+++ app.rb
@@ -0,0 +1,39 @@
+require 'sinatra'
+require 'sqlite3'
+require 'slim'
+require 'bcrypt'
+
+enable :sessions
+
+db = SQLite3::Database.new('db/database.db')
+db.results_as_hash = true
+
+get('/') do
+    slim(:index)
+end
+
+get('/register') do
+    slim(:reg)
+end
+
+post('/create') do
+    name = params["name"]
+    email = params["email"]
+    password = BCrypt::Password.create(params["pass"])
+    db.execute('INSERT INTO users(Name, Email, Password) VALUES(?, ?, ?)', name, email, password) 
+    redirect('/')
+end
+
+post('/login') do
+    result = db.execute("SELECT Password FROM users WHERE Name =(?)", params["name"])
+    if result[0] == nil
+        redirect('/lolno')
+    end
+    not_password = result[0]["Password"]
+    if BCrypt::Password.new(not_password) == params["pass"]
+        session[:loggedin] = true
+        redirect('/welcome')
+    else
+        redirect('/lolno')
+    end
+end
